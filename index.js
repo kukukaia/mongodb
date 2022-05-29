459ca6f34e80084a8dfb320b0724d03958b37b63
kukukaia <kukukaia@protonmail.com>
2022-05-29 01:09:09 -0300

    adicionando arquivos

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose');
-mongoose.connect("")
+mongoose.connect("mongodb+srv://kukukaia:<password>@cluster0.bodoj94.mongodb.net/?retryWrites=true&w=majority")
 
 const Cat = mongoose.model('Cat', { name: String, color: String});
-
-constr kitty = new Cat({
+const kitty = new Cat({
     name: 'Dotty',
     color: 'black',
 });
 
-app.get('/'),(req,res)=>{
+app.get('/',(req,res)=>{
     kitty.save().then(() => res.end('meOOw'));
-}
+    res.end('meoow')
+})
 
 app.listen(3000)
